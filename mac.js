ecdf714143008cd44381bef2a924180d27e9a9be
Kenneth G. Franqueiro <kenneth.franqueiro@gmail.com>
2015-07-09 20:08:10 -0400

    Fix #100: Rename all the Helper apps/executables

--- mac.js
+++ mac.js
@@ -8,6 +8,25 @@ var ncp = require('ncp').ncp
 var series = require('run-series')
 var common = require('./common')
 
+function moveHelpers (frameworksPath, appName, callback) {
+  function rename (basePath, oldName, newName, cb) {
+    mv(path.join(basePath, oldName), path.join(basePath, newName), cb)
+  }
+
+  series([' Helper', ' Helper EH', ' Helper NP'].map(function (suffix) {
+    return function (cb) {
+      var executableBasePath = path.join(frameworksPath, 'Electron' + suffix + '.app', 'Contents', 'MacOS')
+
+      rename(executableBasePath, 'Electron' + suffix, appName + suffix, function (err) {
+        if (err) return cb(err)
+        rename(frameworksPath, 'Electron' + suffix + '.app', appName + suffix + '.app', cb)
+      })
+    }
+  }), function (err) {
+    callback(err)
+  })
+}
+
 module.exports = {
   createApp: function createApp (opts, templatePath, callback) {
     var appRelativePath = path.join('Electron.app', 'Contents', 'Resources', 'app')
@@ -15,9 +34,9 @@ module.exports = {
       if (err) return callback(err)
 
       var contentsPath = path.join(tempPath, 'Electron.app', 'Contents')
-      var helperPath = path.join(contentsPath, 'Frameworks', 'Electron Helper.app')
+      var frameworksPath = path.join(contentsPath, 'Frameworks')
       var appPlistFilename = path.join(contentsPath, 'Info.plist')
-      var helperPlistFilename = path.join(helperPath, 'Contents', 'Info.plist')
+      var helperPlistFilename = path.join(frameworksPath, 'Electron Helper.app', 'Contents', 'Info.plist')
       var appPlist = plist.parse(fs.readFileSync(appPlistFilename).toString())
       var helperPlist = plist.parse(fs.readFileSync(helperPlistFilename).toString())
 
@@ -62,13 +81,10 @@ module.exports = {
         })
       }
 
-      // Move Helper binary, then Helper.app, then top-level .app
+      // Move Helper apps/executables, then top-level .app
       var finalAppPath = path.join(tempPath, opts.name + '.app')
       operations.push(function (cb) {
-        var helperBinaryPath = path.join(helperPath, 'Contents', 'MacOS')
-        mv(path.join(helperBinaryPath, 'Electron Helper'), path.join(helperBinaryPath, opts.name + ' Helper'), cb)
-      }, function (cb) {
-        mv(helperPath, path.join(path.dirname(helperPath), opts.name + ' Helper.app'), cb)
+        moveHelpers(frameworksPath, opts.name, cb)
       }, function (cb) {
         mv(path.dirname(contentsPath), finalAppPath, cb)
       })
