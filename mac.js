erkyrath/electron-packager

Add the `extend-info` and `extra-resource` arguments for creating the Mac application package.
`extend-info`: Take the filename of a plist file and extend Info.plist with its contents. Other explicit arguments such as `app-bundle-id` override this.

`extra-resource`: Take a filename and copy that file into the app's Contents/Resources directory. May be used multiple times.

mac.js
@@ -41,6 +41,16 @@ module.exports = {
       var helperPlist = plist.parse(fs.readFileSync(helperPlistFilename).toString())
 
       // Update plist files
+
+      // If an extend-info file was supplied, copy its contents in first.
+      if (opts['extend-info']) {
+        var extendPlist = plist.parse(fs.readFileSync(opts['extend-info']).toString())
+        for (var key in extendPlist) {
+          appPlist[key] = extendPlist[key]
+        }
+      }
+
+      // Now set fields based on explicit options
       var defaultBundleName = 'com.electron.' + opts.name.toLowerCase().replace(/ /g, '_')
       var appVersion = opts['app-version']
       var buildVersion = opts['build-version']
@@ -60,7 +70,7 @@ module.exports = {
         appPlist.CFBundleVersion = buildVersion
       }
 
-      if (opts.protocols) {
+      if (opts.protocols && opts.protocols.length) {
         appPlist.CFBundleURLTypes = opts.protocols.map(function (protocol) {
           return {
             CFBundleURLName: protocol.name,
@@ -78,6 +88,7 @@ module.exports = {
 
       var operations = []
 
+      // Copy in the icon, if supplied
       if (opts.icon) {
         operations.push(function (cb) {
           common.normalizeExt(opts.icon, '.icns', function (err, icon) {
@@ -87,6 +98,17 @@ module.exports = {
             } else {
               ncp(icon, path.join(contentsPath, 'Resources', 'atom.icns'), cb)
             }
+          })
+        })
+      }
+
+      // Copy in any other extras
+      var extras = opts['extra-resource']
+      if (extras) {
+        if (!Array.isArray(extras)) extras = [extras]
+        extras.forEach(function (val) {
+          operations.push(function (cb) {
+            ncp(val, path.join(contentsPath, 'Resources', path.basename(val)), cb)
           })
         })
       }

electron/packager

mac.js
@@ -51,6 +51,18 @@ module.exports = {
       var helperNPPlist = plist.parse(fs.readFileSync(helperNPPlistFilename).toString())
 
       // Update plist files
+
+      // If an extend-info file was supplied, copy its contents in first
+
+      if (opts['extend-info']) {
+        var extendAppPlist = plist.parse(fs.readFileSync(opts['extend-info']).toString())
+        for (var key in extendAppPlist) {
+          appPlist[key] = extendAppPlist[key]
+        }
+      }
+
+      // Now set fields based on explicit options
+
       var defaultBundleName = 'com.electron.' + opts.name.toLowerCase()
       var appBundleIdentifier = filterCFBundleIdentifier(opts['app-bundle-id'] || defaultBundleName)
       var helperBundleIdentifier = filterCFBundleIdentifier(opts['helper-bundle-id'] || appBundleIdentifier + '.helper')
@@ -84,7 +96,7 @@ module.exports = {
         appPlist.CFBundleVersion = '' + buildVersion
       }
 
-      if (opts.protocols) {
+      if (opts.protocols && opts.protocols.length) {
         appPlist.CFBundleURLTypes = opts.protocols.map(function (protocol) {
           return {
             CFBundleURLName: protocol.name,
@@ -108,6 +120,7 @@ module.exports = {
 
       var operations = []
 
+      // Copy in the icon, if supplied
       if (opts.icon) {
         operations.push(function (cb) {
           common.normalizeExt(opts.icon, '.icns', function (err, icon) {
@@ -117,6 +130,17 @@ module.exports = {
             } else {
               ncp(icon, path.join(contentsPath, 'Resources', 'atom.icns'), cb)
             }
+          })
+        })
+      }
+
+      // Copy in any other extras
+      var extras = opts['extra-resource']
+      if (extras) {
+        if (!Array.isArray(extras)) extras = [extras]
+        extras.forEach(function (val) {
+          operations.push(function (cb) {
+            ncp(val, path.join(contentsPath, 'Resources', path.basename(val)), cb)
           })
         })
       }