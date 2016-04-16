c7dde68480852d0cb6824ba0e910b3e4332720c5
Mark Lee <electronjs@lazymalevolence.com>
2016-04-16 00:47:07 -0700

    Travis: revert back to using a container for Linux builds

--- test/ci/before_install.sh
+++ test/ci/before_install.sh
@@ -2,13 +2,6 @@
 # -*- coding: utf-8 -*-
 
 case "$TRAVIS_OS_NAME" in
-  "linux")
-    sudo dpkg --add-architecture i386
-    # Chrome source does not work because they no longer support i386
-    sudo rm /etc/apt/sources.list.d/google-chrome.list*
-    sudo apt-get update
-    sudo apt-get install --no-install-recommends --yes wine1.6
-    ;;
   "osx")
     # Install Wine
     BREW_INSTALL="brew install --ignore-dependencies --force-bottle"
