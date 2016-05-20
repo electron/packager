258abed507b7e172ba054c3dfbf022ef083bae88
Fred Cox <mcfedr@gmail.com>
2016-05-20 13:29:57 +0300

    Find electron-version in package.json config

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function validateList (list, supported, name) {
 function getNameAndVersion (opts, dir, cb) {
   var props = []
   if (!opts.name) props.push(['productName', 'name'])
-  if (!opts.version) props.push(['dependencies.electron-prebuilt', 'devDependencies.electron-prebuilt', 'electron-version'])
+  if (!opts.version) props.push(['dependencies.electron-prebuilt', 'devDependencies.electron-prebuilt', 'config.electron-version'])
 
   // Name and version provided, no need to infer
   if (props.length === 0) return cb(null)
