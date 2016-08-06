459b63508fc85bb0cf62abb46d59f96037741e7a
Mark Lee <electronjs@lazymalevolence.com>
2016-08-06 12:09:40 -0700

    Tests: pre-download Electron 1.3.1

--- test/index.js
+++ test/index.js
@@ -5,16 +5,25 @@ const exec = require('child_process').exec
 const series = require('run-series')
 const util = require('./util')
 
-// Download all Electron distributions before running tests to avoid timing out due to network speed.
-// Most tests run with the config.json version, but we have a special test (in mac.js) for 0.37.4.
-series([
+// Download all Electron distributions before running tests to avoid timing out due to network
+// speed. Most tests run with the config.json version, but we have some tests using 0.37.4, and an
+// electron module specific test using 1.3.1.
+function preDownloadElectron () {
+  const versions = [
+    config.version,
+    '0.37.4',
+    '1.3.1'
+  ]
+  return versions.map((version) => {
+    return (cb) => {
+      console.log(`Calling electron-download for ${version} before running tests...`)
+      util.downloadAll(version, cb)
+    }
+  })
+}
+
+series(preDownloadElectron().concat([
   function (cb) {
-    console.log('Calling electron-download before running tests...')
-    util.downloadAll(config.version, cb)
-  }, function (cb) {
-    console.log('Calling electron-download 0.37.4 before running tests...')
-    util.downloadAll('0.37.4', cb)
-  }, function (cb) {
     console.log('Running npm install in fixtures/basic...')
     exec('npm install', {cwd: util.fixtureSubdir('basic')}, cb)
   }, function (cb) {
@@ -24,7 +33,7 @@ series([
     console.log('Running npm install in fixtures/el-0374...')
     exec('npm install', {cwd: util.fixtureSubdir('el-0374')}, cb)
   }
-], function () {
+]), function () {
   console.log('Running tests...')
   require('./basic')
   require('./asar')
