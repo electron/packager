20741d43d3bc201e4c1422baf503c46be6603711
Rahat Ahmed <rahatarmanahmed@gmail.com>
2016-08-08 19:33:38 -0500

    Remove unnecessary second get-package-json call

--- index.js
+++ index.js
@@ -51,38 +51,37 @@ function validateList (list, supported, name) {
 function getNameAndVersion (opts, dir, cb) {
   var props = []
   if (!opts.name) props.push(['productName', 'name'])
-  if (!opts.version) props.push(['dependencies.electron', 'devDependencies.electron'])
+  if (!opts.version) {
+    props.push([
+      'dependencies.electron',
+      'devDependencies.electron',
+      'dependencies.electron-prebuilt',
+      'devDependencies.electron-prebuilt'
+    ])
+  }
 
   // Name and version provided, no need to infer
   if (props.length === 0) return cb(null)
 
   // Search package.json files to infer name and version from
   getPackageInfo(props, dir, function (err, result) {
-    if (err) {
-      // `get-package-info` exploded looking for `electron`. Try `electron-prebuilt` instead
-      props.pop()
-      props.push(['dependencies.electron-prebuilt', 'devDependencies.electron-prebuilt'])
-      getPackageInfo(props, dir, function (err, result) {
-        if (err) return cb(err)
-        return inferNameAndVersionFromInstalled('electron-prebuilt', opts, result, cb)
-      })
-    } else {
-      return inferNameAndVersionFromInstalled('electron', opts, result, cb)
-    }
+    if (err) return cb(err)
+    return inferNameAndVersionFromInstalled(opts, result, cb)
   })
 }
 
-function inferNameAndVersionFromInstalled (packageName, opts, result, cb) {
+function inferNameAndVersionFromInstalled (opts, result, cb) {
   if (result.values.productName) {
     debug('Inferring application name from productName or name in package.json')
     opts.name = result.values.productName
   }
-  if (result.values[`dependencies.${packageName}`]) {
+  if (result.values['dependencies.electron']) {
+    var packageName = result.source['dependencies.electron'].prop.split('.')[1]
     resolve(packageName, {
-      basedir: path.dirname(result.source[`dependencies.${packageName}`].src)
+      basedir: path.dirname(result.source['dependencies.electron'].src)
     }, function (err, res, pkg) {
       if (err) return cb(err)
-      debug(`Inferring target Electron version from ${packageName} dependency or devDependency in package.json`)
+      debug(`Inferring target Electron version from ${result.source['dependencies.electron'].prop} in package.json`)
       opts.version = pkg.version
       return cb(null)
     })
