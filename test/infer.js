a675c4f6c3ae90b66b411f680d9db6036d2ddb63
Mark Lee <electronjs@lazymalevolence.com>
2017-03-16 21:03:34 -0400

    Replace waterfall with Promises in infer tests

--- test/infer.js
+++ test/infer.js
@@ -6,9 +6,9 @@ const getMetadataFromPackageJSON = require('../infer')
 const os = require('os')
 const packager = require('..')
 const path = require('path')
+const pify = require('pify')
 const pkgUp = require('pkg-up')
 const util = require('./util')
-const waterfall = require('run-waterfall')
 
 function createInferElectronVersionTest (fixture, packageName) {
   return (opts) => {
@@ -19,77 +19,64 @@ function createInferElectronVersionTest (fixture, packageName) {
       delete opts.electronVersion
       opts.dir = path.join(__dirname, 'fixtures', fixture)
 
-      waterfall([
-        (cb) => {
-          getMetadataFromPackageJSON(opts, opts.dir, cb)
-        }, (cb) => {
-          fs.readFile(path.join(opts.dir, 'package.json'), cb)
-        }, (pkg, cb) => {
-          const packageJSON = JSON.parse(pkg)
+      pify(getMetadataFromPackageJSON)(opts, opts.dir)
+        .then((pkg) => {
+          const packageJSON = require(path.join(opts.dir, 'package.json'))
           t.equal(opts.electronVersion, packageJSON.devDependencies[packageName], `The version should be inferred from installed ${packageName} version`)
-          cb()
-        }
-      ], (err) => {
-        t.end(err)
-      })
+          return t.end()
+        }).catch(t.end)
     }
   }
 }
 
-function copyFixtureToTempDir (fixtureSubdir, cb) {
+function copyFixtureToTempDir (fixtureSubdir) {
   let tmpdir = path.join(os.tmpdir(), fixtureSubdir)
   let fixtureDir = path.join(__dirname, 'fixtures', fixtureSubdir)
-  waterfall([
-    cb => {
-      let tmpdirPkg = pkgUp.sync(path.join(tmpdir, '..'))
-      if (tmpdirPkg) return cb(new Error(`Found package.json in parent of temp directory, which will interfere with test results. Please remove package.json at ${tmpdirPkg}`))
-      cb()
-    },
-    cb => fs.emptyDir(tmpdir, cb),
-    (cb1, cb2) => fs.copy(fixtureDir, tmpdir, cb2 || cb1), // inconsistent cb arguments from fs.emptyDir
-    cb => cb(null, tmpdir)
-  ], cb)
+  let tmpdirPkg = pkgUp.sync(path.join(tmpdir, '..'))
+
+  if (tmpdirPkg) {
+    throw new Error(`Found package.json in parent of temp directory, which will interfere with test results. Please remove package.json at ${tmpdirPkg}`)
+  }
+
+  return fs.emptyDir(tmpdir)
+    .then(() => fs.copy(fixtureDir, tmpdir))
+    .then(() => tmpdir)
 }
 
 function createInferFailureTest (opts, fixtureSubdir) {
-  return function (t) {
+  return (t) => {
     t.timeoutAfter(config.timeout)
 
-    copyFixtureToTempDir(fixtureSubdir, (err, dir) => {
-      if (err) return t.end(err)
+    copyFixtureToTempDir(fixtureSubdir)
+      .then((dir) => {
+        delete opts.electronVersion
+        opts.dir = dir
 
-      delete opts.electronVersion
-      opts.dir = dir
-
-      packager(opts, function (err, paths) {
-        t.ok(err, 'error thrown')
-        t.end()
-      })
-    })
+        return pify(packager)(opts)
+      }).then(
+        paths => t.end('expected error'),
+        err => {
+          t.ok(err, 'error thrown')
+          return t.end()
+        }
+      ).catch(t.end)
   }
 }
 
 function createInferMissingVersionTest (opts) {
   return (t) => {
     t.timeoutAfter(config.timeout)
-    waterfall([
-      (cb) => {
-        copyFixtureToTempDir('infer-missing-version-only', cb)
-      }, (dir, cb) => {
+    copyFixtureToTempDir('infer-missing-version-only')
+      .then((dir) => {
         delete opts.electronVersion
         opts.dir = dir
 
-        getMetadataFromPackageJSON(opts, dir, cb)
-      }, (cb) => {
-        fs.readFile(path.join(opts.dir, 'package.json'), cb)
-      }, (pkg, cb) => {
-        const packageJSON = JSON.parse(pkg)
+        return pify(getMetadataFromPackageJSON)(opts, dir)
+      }).then(() => {
+        const packageJSON = require(path.join(opts.dir, 'package.json'))
         t.equal(opts.electronVersion, packageJSON.devDependencies['electron'], 'The version should be inferred from installed electron module version')
-        cb()
-      }
-    ], (err) => {
-      t.end(err)
-    })
+        return t.end()
+      }).catch(t.end)
   }
 }
 
