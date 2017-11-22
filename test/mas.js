7ca8fd5438865999f6254e724dc15e8a36b1b3e7
Mark Lee <electronjs@lazymalevolence.com>
2017-11-21 23:46:10 -0800

    No need to run darwin tests for mas builds, it's the same codepath

--- test/mas.js
+++ test/mas.js
@@ -1,9 +1,10 @@
 'use strict'
 
 const config = require('./config.json')
+const packager = require('..')
 const util = require('./util')
 
-var baseOpts = {
+const masOpts = {
   name: 'basicTest',
   dir: util.fixtureSubdir('basic'),
   electronVersion: config.version,
@@ -11,4 +12,20 @@ var baseOpts = {
   platform: 'mas'
 }
 
-require('./mac')(baseOpts)
+util.packagerTest('warn if building for mas and not signing', t => {
+  const warningLog = console.warn
+  let output = ''
+  console.warn = message => { output += message }
+
+  const finalize = err => {
+    console.warn = warningLog
+    t.end(err)
+  }
+
+  packager(masOpts)
+    .then(() => {
+      t.ok(output.match(/signing is required for mas builds/), 'the correct warning is emitted')
+      return null
+    }).then(finalize)
+    .catch(finalize)
+})
