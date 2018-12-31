e58cad641177155c25f52ab6163a60a7e18abfd4
Mark Lee <electronjs@lazymalevolence.com>
2018-12-31 10:30:54 -0800

    Travis CI: Xenial changes

--- test/ci/before_install.sh
+++ test/ci/before_install.sh
@@ -3,9 +3,6 @@
 
 case "$TRAVIS_OS_NAME" in
   "linux")
-    # Not using Trusty containers because it can't install wine1.6(-i386),
-    # see: https://github.com/travis-ci/travis-ci/issues/6460
-    sudo rm /etc/apt/sources.list.d/google-chrome.list
     sudo apt-key adv --fetch-keys http://dl.yarnpkg.com/debian/pubkey.gpg
     echo "deb http://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
     sudo dpkg --add-architecture i386
