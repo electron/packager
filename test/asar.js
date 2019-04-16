30c56f4fab2f3a2711ef6f29428734d9bc97a91b
Mark Lee <malept@users.noreply.github.com>
2019-04-15 18:58:13 -0700

    Refactor test helpers (#977)
    Refactor the test helpers so that it's easier to isolate tests to run when developing.

--- test/asar.js
+++ test/asar.js
@@ -18,21 +18,35 @@ test('asar argument test: asar is not an Object or a bool', t => {
   t.false(common.createAsarOpts({ asar: 'string' }), 'createAsarOpts returns false')
 })
 
-util.testSinglePlatform('default_app.asar removal test', async (t, opts) => {
+test.serial('default_app.asar removal test', util.testSinglePlatform(async (t, opts) => {
   opts.name = 'default_appASARTest'
   opts.dir = util.fixtureSubdir('el-0374')
   opts.electronVersion = '0.37.4'
 
   const resourcesPath = await util.packageAndEnsureResourcesPath(t, opts)
   await util.assertPathNotExists(t, path.join(resourcesPath, 'default_app.asar'), 'The output directory should not contain the Electron default_app.asar file')
-})
+}))
 
 async function assertUnpackedAsar (t, resourcesPath) {
   await util.assertDirectory(t, path.join(resourcesPath, 'app.asar.unpacked'), 'app.asar.unpacked should exist under the resources subdirectory when opts.asar_unpack is set')
   await util.assertDirectory(t, path.join(resourcesPath, 'app.asar.unpacked', 'dir_to_unpack'), 'dir_to_unpack should exist under app.asar.unpacked subdirectory when opts.asar-unpack-dir is set dir_to_unpack')
 }
 
-util.testSinglePlatform('asar test', async (t, opts) => {
+function failedPrebuiltAsarTest (extraOpts, errorRegex) {
+  const dir = util.fixtureSubdir('asar-prebuilt')
+  return util.invalidOptionTest({
+    name: 'prebuiltAsarFailingTest',
+    dir: dir,
+    prebuiltAsar: path.join(dir, 'app.asar'),
+    ...extraOpts
+  }, errorRegex)
+}
+
+function incompatibleOptionWithPrebuiltAsarTest (extraOpts) {
+  return failedPrebuiltAsarTest(extraOpts, /is incompatible with prebuiltAsar/)
+}
+
+test.serial('asar test', util.testSinglePlatform(async (t, opts) => {
   opts.name = 'asarTest'
   opts.dir = util.fixtureSubdir('basic')
   opts.asar = {
@@ -46,9 +60,9 @@ util.testSinglePlatform('asar test', async (t, opts) => {
     util.assertPathNotExists(t, path.join(resourcesPath, 'app'), 'app subdirectory should NOT exist when app.asar is built'),
     assertUnpackedAsar(t, resourcesPath)
   ])
-})
+}))
 
-util.testSinglePlatform('prebuilt asar test', async (t, opts) => {
+test.serial('prebuilt asar test', util.testSinglePlatform(async (t, opts) => {
   util.setupConsoleWarnSpy()
   opts.name = 'prebuiltAsarTest'
   opts.dir = util.fixtureSubdir('asar-prebuilt')
@@ -70,22 +84,8 @@ util.testSinglePlatform('prebuilt asar test', async (t, opts) => {
   await util.assertFile(t, path.join(resourcesPath, 'app.asar'), 'app.asar should exist under the resources subdirectory when opts.prebuiltAsar points to a prebuilt asar')
   await util.assertFilesEqual(t, opts.prebuiltAsar, path.join(resourcesPath, 'app.asar'), 'app.asar should equal the prebuilt asar')
   await util.assertPathNotExists(t, path.join(resourcesPath, 'app'), 'app subdirectory should NOT exist when app.asar is built')
-})
+}))
 
-function testFailedPrebuiltAsar (name, extraOpts, errorRegex) {
-  const dir = util.fixtureSubdir('asar-prebuilt')
-  util.testSinglePlatform(`prebuilt asar: fail on ${name}`, util.invalidOptionTest({
-    name: 'prebuiltAsarFailingTest',
-    dir: dir,
-    prebuiltAsar: path.join(dir, 'app.asar'),
-    ...extraOpts
-  }, errorRegex))
-}
-
-function testIncompatibleOptionWithPrebuiltAsar (extraOpts) {
-  testFailedPrebuiltAsar(`specifying prebuiltAsar and ${Object.keys(extraOpts).join(',')}`, extraOpts, /is incompatible with prebuiltAsar/)
-}
-
-testFailedPrebuiltAsar('prebuiltAsar set to directory', { prebuiltAsar: util.fixtureSubdir('asar-prebuilt') }, /must be an asar file/)
-testIncompatibleOptionWithPrebuiltAsar({ afterCopy: [] })
-testIncompatibleOptionWithPrebuiltAsar({ afterPrune: [] })
+test('prebuiltAsar: fail when set to directory', failedPrebuiltAsarTest({ prebuiltAsar: util.fixtureSubdir('asar-prebuilt') }, /must be an asar file/))
+test('prebuiltAsar: fail when specifying afterCopy', incompatibleOptionWithPrebuiltAsarTest({ afterCopy: [] }))
+test('prebuiltAsar: fail when specifying afterPrune', incompatibleOptionWithPrebuiltAsarTest({ afterPrune: [] }))
