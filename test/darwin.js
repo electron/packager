f1a6b00c2c776aaf20c1f468535118fb4b963757
Mark Lee <electronjs@lazymalevolence.com>
2019-06-05 11:17:06 -0700

    Do not run spctl in tests

--- test/darwin.js
+++ test/darwin.js
@@ -294,7 +294,7 @@ if (!(process.env.CI && process.platform === 'win32')) {
 
   if (process.platform === 'darwin') {
     test.serial('end-to-end codesign', darwinTest(async (t, opts) => {
-      opts.osxSign = { identity: 'codesign.electronjs.org', verbose: 4 }
+      opts.osxSign = { identity: 'codesign.electronjs.org', 'gatekeeper-assess': false }
 
       const finalPath = (await packager(opts))[0]
       const appPath = path.join(finalPath, opts.name + '.app')
