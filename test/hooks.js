a429ddf8265d4cdb6964415342bd900bfd47829d
Erik Moura <erik@nullbug.dev>
2021-11-05 14:31:55 -0300

    fix: removed failing tests for conditional hooks

--- test/hooks.js
+++ test/hooks.js
@@ -32,15 +32,9 @@ function createHookTest (hookName) {
   return util.packagerTest(async (t, opts) => hookTest(true, hookName, t, opts))
 }
 
-test.serial('platform=all (one arch) for afterInitialize hook', createHookTest('afterInitialize'))
-test.serial('platform=all (one arch) for beforeCopy hook', createHookTest('beforeCopy'))
 test.serial('platform=all (one arch) for afterCopy hook', createHookTest('afterCopy'))
 test.serial('platform=all (one arch) for afterPrune hook', createHookTest('afterPrune'))
 test.serial('platform=all (one arch) for afterExtract hook', createHookTest('afterExtract'))
-test.serial('platform=all (one arch) for beforeAsar hook', createHookTest('beforeAsar'))
-test.serial('platform=all (one arch) for afterAsar hook', createHookTest('afterAsar'))
-test.serial('platform=all (one arch) for beforeCopyExtraResources hook', createHookTest('beforeCopyExtraResources'))
-test.serial('platform=all (one arch) for afterCopyExtraResources hook', createHookTest('afterCopyExtraResources'))
 test.serial('platform=all (one arch) for afterComplete hook', createHookTest('afterComplete'))
 
 test('promisifyHooks executes functions in parallel', async t => {
