211404ab51e0857653d502b7f00bda707a9ec61e
Erick Zhao <erick@hotmail.ca>
2022-08-23 15:39:22 -0700

    add universal to tsd tests

--- test/index.test-d.ts
+++ test/index.test-d.ts
@@ -52,6 +52,18 @@ await packager({
 await packager({
   dir: '.',
   name: 'myapplication',
+  platform: 'darwin',
+  arch: 'universal',
+  electronVersion: '0.34.0',
+  osxUniversal: {
+    mergeASARs: true
+  }
+});
+
+
+await packager({
+  dir: '.',
+  name: 'myapplication',
   platform: 'all',
   arch: 'all',
   electronVersion: '0.34.0',
