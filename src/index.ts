f779f0bb356d397b45506f5840643d7433467eb0
Erik Moura <erikian@electronjs.org>
2023-11-19 19:07:52 -0300

    chore: make `allOfficialArchsForPlatformAndVersion` a public API

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { serialHooks } from './hooks';
 import { packager } from './packager';
+import { allOfficialArchsForPlatformAndVersion } from './targets';
 
 export default packager;
 
-export { packager, serialHooks };
+export { allOfficialArchsForPlatformAndVersion, packager, serialHooks };
 
 export * from './types';
